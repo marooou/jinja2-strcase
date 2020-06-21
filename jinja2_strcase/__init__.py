38e60c074f611f2900a40858d6257a90115cf295
Marek Chmiel <marek.chmiel@karhoo.com>
2020-06-21 18:52:44 +0200

    Add strcase initial implementation

--- jinja2_strcase/__init__.py
+++ jinja2_strcase/__init__.py
@@ -4,6 +4,6 @@
 from .jinja2_strcase import StrcaseExtension
 
 __author__ = 'Marek Chmiel'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 __all__ = ['StrcaseExtension']
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='jinja2-strcase',
-    version='0.0.1',
+    version='0.0.2',
     author='Marek Chmiel',
     maintainer='Marek Chmiel',
     license='MIT',
